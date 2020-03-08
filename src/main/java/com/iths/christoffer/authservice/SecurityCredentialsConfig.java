e1e7dd2ab7601eca8d5c20892fddc9755233cf90
Christoffer Clausen <christoffer.clausen@iths.se>
2020-03-08 18:43:36 +0100

    fixed to work with gateway

--- src/main/java/com/iths/christoffer/authservice/SecurityCredentialsConfig.java
+++ src/main/java/com/iths/christoffer/authservice/SecurityCredentialsConfig.java
@@ -27,17 +27,6 @@ public class SecurityCredentialsConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .cors().and()
-//                .csrf().disable()
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .exceptionHandling().authenticationEntryPoint((request, response, e) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
-//                .and()
-//                .addFilter(new JwtUsernameAndPasswordAuthenticationFIlter(authenticationManager(), jwtConfig))
-//                .authorizeRequests()
-//                .antMatchers("/admin/**").hasRole("ADMIN")
-
         http
                 .csrf().disable()
                 // make sure we use stateless session; session won't be used to store user's state.
@@ -48,7 +37,6 @@ public class SecurityCredentialsConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 // Add a filter to validate the tokens with every request
                 .addFilter(new JwtUsernameAndPasswordAuthenticationFIlter(authenticationManager(), jwtConfig))
-//                .addFilterAfter(new JwtTokenAuthenticationFilter(jwtConfig), UsernamePasswordAuthenticationFilter.class)
                 // authorization requests config
                 .authorizeRequests()
                 // allow all who are accessing "auth" service
@@ -57,14 +45,6 @@ public class SecurityCredentialsConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/gallery" + "/admin/**").hasRole("ADMIN")
                 // Any other request must be authenticated
                 .anyRequest().authenticated();
-
-
-//                .antMatchers(HttpMethod.POST, jwtConfig.getUri()).permitAll()
-//                .antMatchers(HttpMethod.POST, jwtConfig.getUri()).hasRole("ADMIN")//.permitAll()
-//                .antMatchers(HttpMethod.PATCH, jwtConfig.getUri()).hasRole("ADMIN")//.permitAll()
-//                .antMatchers(HttpMethod.PUT, jwtConfig.getUri()).hasRole("ADMIN")//.permitAll()
-//                .antMatchers(HttpMethod.DELETE, jwtConfig.getUri()).hasRole("ADMIN")//.permitAll()
-//                .anyRequest().authenticated();
     }
 
     @Override
